1853e71b3f9927d969221fe0251cd0d8054aa8f2
Hasana Manandhar <manandharhasana07@gmail.com>
2024-10-07 23:14:32 +0545

    Tried to show active page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
 const nav = document.querySelector("#navBar");
 const head = document.querySelector("header");
+const navLinks = nav.querySelectorAll("a");
+const sections = document.querySelectorAll("section");
+
+function updateActiveLink() {
+    let currentActive = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            currentActive = section.getAttribute("id");
+            console.log(currentActive);
+        }
+    });
+}
+window.addEventListener("scroll", updateActiveLink);
+window.addEventListener("load", updateActiveLink);
+
+const currentUrl = window.location.href;
+console.log(currentUrl); // Get the current URL
+
+if (currentUrl.includes("skillsContents")) {
+    active = "skills";
+} else if (currentUrl.includes("galleryContents")) {
+    active = "gallery";
+} else if (currentUrl.includes("highlightsContents")) {
+    active = "highlights";
+} else if (currentUrl.includes("blog")) {
+    active = "blog";
+}
+
+navLinks.forEach((link) => {
+    console.log(link.getAttribute("data-active"));
+    if (link.getAttribute("data-active") === "true") {
+        // link.classList.add("Active"); // Add the enlarge class
+        link.style.border = "2px solid orange";
+    }
+});
 
 window.addEventListener("scroll", () => {
-const navButtons = nav.querySelectorAll("a");
     if (
         document.body.scrollTop !== 0 ||
         document.documentElement.scrollTop !== 0
     ) {
-        navButtons.forEach((link) => {
+        navLinks.forEach((link) => {
             link.style.fontSize = "20px";
         });
         head.style.backgroundColor = "";
@@ -18,7 +55,7 @@ const navButtons = nav.querySelectorAll("a");
         head.style.backgroundColor = "";
         head.style.paddingTop = "50px";
         head.style.paddingBottom = "50px";
-        navButtons.forEach((link) => {
+        navLinks.forEach((link) => {
             link.style.fontSize = "25px";
         });
     }
